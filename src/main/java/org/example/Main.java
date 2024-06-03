054cb138b8b6a86b9f50473cbbf838a32d014d2b
liutku5 <l1utkuz20@gmail.com>
2024-06-03 10:37:49 +0300

    pirma uzklausa

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,33 @@ package org.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        connect();
+
+        Connection con = connect();
+
+        String query = "SELECT * FROM authors ";
+        try {
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while ((rs.next())) {
+                System.out.println(rs.getLong("id") + " " + rs.getString("name") + " " + rs.getString("surname"));
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
     }
 
-    public static Connection connect(){
+    public static Connection connect() {
         Connection connection = null;
-        try{
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","");
-        }catch (Exception e){
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root", "");
+        } catch (Exception e) {
             System.out.println("Failed to connect to database");
         }
         return connection;
